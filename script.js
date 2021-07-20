909af83837973da5d4040d5a96ce32e1d6c69f5e
Kiishi <kiishioluwa@gmail.com>
2021-07-20 15:49:23 +0100

    personalized the website

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let mode = document.getElementById('theme-switch');
+let body = document.querySelector('dark-mode');
+
+
 function response(){
     let x = document.getElementById('home');
     if(x.className === 'navigation'){
@@ -5,5 +9,12 @@ function response(){
     }
     else{
         x.className = 'navigation'
+    }
+}
+
+const switchTheme = () =>{
+    if(document.body.className === 'dark-mode'){
+        // console.log('sup');
+        document.body.className = 'light-mode';
     }
 }
